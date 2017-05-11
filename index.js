61aae4caf62e4aa17325a0c481e5ae517fe47cc4
Michal Glaus <misogl@users.noreply.github.com>
2017-05-11 18:50:12 +0200

    Add react/no-deprecated rule
    https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ module.exports = {
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
     'react/jsx-wrap-multilines': 2,
+    'react/no-deprecated': 2,
     'react/no-did-mount-set-state': 2,
     'react/no-did-update-set-state': 2,
     'react/no-unknown-property': 2,
